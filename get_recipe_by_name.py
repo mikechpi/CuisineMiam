e52fe4d5a72912b9e5b7a497f1fee03e2e1349b1
Aurel Yahouedeou <admofficiel@gmail.com>
2024-03-06 14:51:16 +0100

    Corrections erreurs via flake8

--- get_recipe_by_name.py
+++ get_recipe_by_name.py
@@ -1,24 +1,25 @@
 import requests
-#La bibliothèque 'requests' permet de faire des requêtes HTTP depuis Python
+# La bibliothèque 'requests' permet de faire des requêtes HTTP depuis Python
 
 
+# Chargement des variables d'environnement depuis un fichier .env
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_recipe_by_name(api_key, recipe_name): #La fonction get_recipe_by_name prend en entrée une clé API et le nom d'une recette
-    base_url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=9c60bb8a&app_key=46df1aff8431edce2ce1f40a0b9bb08e&q=chiken"
-    #base_url = "https://api.spoonacular.com/recipes/findByIngredients"
-    params = {
-        'q': recipe_name,
-        'type': 'public',
-        'app_id': '9c60bb8a',
-        'app_key': '46df1aff8431edce2ce1f40a0b9bb08e',
-        'random': 'true'
-    }
+def get_recipe_by_name(api_key, recipe_name):
+    # La fonction get_recipe_by_name prend en entrée une
+    # clé API et le nom d'une recette
+    base_url = (
+        "https://api.edamam.com/api/recipes/v2"
+        "?type=public&app_id=9c60bb8a&app_key=46df1aff8431edce2ce1f40a0b9bb08e"
+    )
+    # base_url = "https://api.spoonacular.com/recipes/findByIngredients"
+    params = {"q": recipe_name}
 
-    response = requests.get(base_url, params=params) 
-    #envoie une requête GET à l'API Edamam avec les paramètres spécifiés, et stocke la réponse dans une variable 'response'
+    response = requests.get(base_url, params=params)
+    # envoie une requête GET à l'API Edamam avec les paramètres spécifiés,
+    # et stocke la réponse dans une variable 'response'
 
     if response.status_code == 200:
         data = response.json()
@@ -31,14 +32,18 @@ def get_recipe_by_name(api_key, recipe_name): #La fonction get_recipe_by_name pr
         print(f"Erreur lors de la requête à l'API: {response.status_code}")
         return None
 
-# Exemple d'utilisation
-ingredient = ["poulet", "oignon"]
-api_key = "46df1aff8431edce2ce1f40a0b9bb08e"
-# api_key = "46df1aff8431edce2ce1f40a0b9bb08e"
-recipe_name = "tarte aux pommes"
-recipe = get_recipe_by_name(api_key, recipe_name)
 
-if recipe:
-    print(f"Nom de la recette: {recipe['label']}")
-    print(f"Ingrédients: {recipe['ingredientLines']}")
-    print(f"Instructions: {recipe['url']}")
+# L'utilisateur va être invité à entrer le nom d'une recette.
+# La fonction get_recipe_by_name est alors appelée avec la clé API
+# et le nom de la recette. Si une recette correspondant au nom spécifié
+# est trouvée, les informations de la recette sont affichées.
+# Sinon, un message d'erreur est affiché.
+api_key = "46df1aff8431edce2ce1f40a0b9bb08e"
+
+if __name__ == '__main__':
+    recipe_name = input("Quelle recette cherchez-vous ? ")
+    recipe = get_recipe_by_name(api_key, recipe_name)
+    if recipe:
+        print(f"Nom de la recette: {recipe['label']}")
+        print(f"Ingrédients: {recipe['ingredientLines']}")
+        print(f"Instructions: {recipe['url']}")
---
+++ tests/test_get_recipe_by_name.py
@@ -0,0 +1,25 @@
+
+from get_recipe_by_name import get_recipe_by_name
+
+
+def test_get_recipe_by_name():
+    api_key = "46df1aff8431edce2ce1f40a0b9bb08e"
+    recipes_to_test = [
+        ("pizza", {"label": "Pizza Dough"}),
+        ("salade", {"label": "Salade Aux Lardons"}),
+        ("tarte", {"label": "Baby Beet Tarte Tatin from 'River Cottage Veg'"}),
+        ("pâtes", {"label": "(Very) Peppery Pate"}),
+        ("recette inexistante", None)
+    ]
+
+    for recipe_name, expected_result in recipes_to_test:
+        result = get_recipe_by_name(api_key, recipe_name)
+
+        if expected_result is None:
+            assert result is None, (
+                f"La recette '{recipe_name}' devrait renvoyer None")
+        else:
+            assert result is not None, (
+                f"La recette '{recipe_name}' ne devrait pas renvoyer None")
+            assert result['label'] == expected_result['label'], (
+                f"Le nom de la recette '{recipe_name}' ne correspond pas")
